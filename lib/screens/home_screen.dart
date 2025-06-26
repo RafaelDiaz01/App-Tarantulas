7497736cfbe9619b8f261c02ab7fa84495da6f95
RafaelDiaz01 <rafaeldiazmx98@gmail.com>
2025-06-26 14:07:39 -0600

    Se agregó una animación de araña para la pantalla de home, también se cambiaron los colores y la tipografía.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 // Pantalla principal de la aplicaciónimport 'package:flutter/material.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class MyHomeScreen extends StatelessWidget {
   const MyHomeScreen({super.key, required this.title});
@@ -9,22 +10,31 @@ class MyHomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 252, 163, 17),
-        title: Text("Tarantulas", style: TextStyle(fontSize: 30)),
-        actions: [
-          IconButton.outlined(
-            onPressed: () {
-              // Navigator.of(
-              //   context,
-              // ).push(MaterialPageRoute(builder: (context) => AddPlaceScreen()));
-            },
-            icon: Icon(Icons.info_outline, size: 30, color: Colors.black),
-            tooltip: "Créditos", 
+      body: Stack(
+        children: [
+          // Animación de fondo (ajusta opacity si es necesario)
+          Opacity(
+            opacity: 0.3,
+            child: Center(
+              child: SizedBox(
+                width:
+                    MediaQuery.of(context).size.width *
+                    0.6, // 60% del ancho de pantalla
+                height:
+                    MediaQuery.of(context).size.height *
+                    0.4, // 40% del alto de pantalla
+                child: Lottie.asset(
+                  'assets/animations/tarantulas.json',
+                  fit: BoxFit.contain,
+                  repeat: true,
+                ),
+              ),
+            ),
           ),
+          // Contenido encima (botones, texto, etc.)
+          Center(child: Text("¡Encuesta de Tarántulas!")),
         ],
       ),
-      body: Center(child: Text("Pantalla de inicio")),
     );
   }
 }
