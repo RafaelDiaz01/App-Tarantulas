935b10112282d50a6e0515fd54cb2a7b4143d92a
RafaelDiaz01 <rafaeldiazmx98@gmail.com>
2025-06-26 16:29:53 -0600

    Se subieron cambios respecto a la pantalla de inicio, como cambio de animacion, se utilizaron los tipos de letra de app_theme. También se agregó el leading para realizar configuraciones.

--- lib/main.dart
+++ lib/main.dart
@@ -2,23 +2,39 @@ import 'package:flutter/material.dart';
 import 'package:app_tarantulas/screens/home_screen.dart';
 import 'package:app_tarantulas/configurations/app_theme.dart';
 
-
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  ThemeMode _themeMode = ThemeMode.dark;
+
+  void _toggleTheme() {
+    setState(() {
+      _themeMode =
+          _themeMode == ThemeMode.dark ? ThemeMode.light : ThemeMode.dark;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'App Tarantulas',
-      themeMode: ThemeMode.dark, // Modo oscuro por defecto
-      theme: appTheme,
-      debugShowCheckedModeBanner: false, // Desactiva el banner de debug
-      home: const MyHomeScreen(title: 'Tliltocatl Schroederi'),
+      themeMode: _themeMode,
+      theme: appTheme.copyWith(brightness: Brightness.light), // Tema claro
+      darkTheme: appTheme.copyWith(brightness: Brightness.dark), // Tema oscuro
+      debugShowCheckedModeBanner: false,
+      home: MyHomeScreen(
+        title: 'Tliltocatl Schroederi',
+        onToggleTheme: _toggleTheme, // Pasamos la función al home
+      ),
     );
   }
 }
